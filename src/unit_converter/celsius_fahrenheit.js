d57ba216e89aa62d57c223dfda2155db672300b6
Cristopher Duran <codex8424@gmail.com>
2024-01-06 22:47:37 -0400

    Unit converter add (°C-°F), (K to miles)

---
+++ src/unit_converter/celsius_fahrenheit.js
@@ -0,0 +1,6 @@
+// Celsius a Fahrenheit.
+
+let celsius = 15;
+let Fahrenheit = celsius * 9 / 5 + 32;
+console.log(Fahrenheit + ' °F');
+
---
+++ src/unit_converter/kilometers_miles.js
@@ -0,0 +1,5 @@
+// kilómetros a millas 
+
+let Kilometers = 25;
+let miles = Kilometers / 1.609;
+console.log(miles + ' millas');
