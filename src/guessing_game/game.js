16d8df8f625b1972df05bf3124e41a4e0d460658
Cristopher Duran <codex8424@gmail.com>
2024-01-08 21:55:10 -0400

    added to-do-list and guessing game beta

---
+++ src/guessing_game/game.js
@@ -0,0 +1,13 @@
+// juego de adivinanzas // continuo al dia siguiente
+
+let numberUser = number;
+let numberRandom = Math.floor(Math.random() * 100) + 1;
+
+console.log(numberRandom);
+/*
+if(numberRandom === number){
+    console.log(number + ' = ' + numberRandom + ' Adivinaste el numero');
+}else{
+    console.log(number + ' = ' + numberRandom + ' No adivinaste');
+}
+*/
